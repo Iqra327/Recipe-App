c87606acea56a191224c2e5141326169c489d4d7
Iqra327 <iqrarasheed219@gmail.com>
2024-08-16 23:04:47 +0500

    fetched data

--- script.js
+++ script.js
@@ -16,15 +16,43 @@ const html = `
   </div>
 `
 
-searchBox.addEventListener('input', () => {
-  const inputRecipe = searchBox.value;
-});
+async function getRecipe(recipeName){
+  try {
+    if(recipeName.trim() != '')
+    {
+      console.log('fetching');
+      const data =await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${recipeName}`);
+      const response =await data.json();
 
-function getRecipe(){
+      response.meals.forEach(meal => {
+        const html = `
+          <div class="col-lg-3 col-md-4 col-sm-6 mb-3">
+            <div class="card pb-3">
+              <img class="card-img-top" src="${meal.strMealThumb}" alt="Card image cap">
+              <div class="card-body text-center">
+                <h5 class="card-title">${meal.strMeal}</h5>
+                <p class="card-text">${meal.strArea} Dish <br>Belongs to ${meal.strCategory} category</p>
+                <a href="#" class="btn btn-view-recipe js-view-recipe-btn">View Recipe</a>
+              </div>
+            </div>
+          </div>
+        `
+        recipeContainer.innerHTML += html;       
+      console.log(meal);
+      });
+    }else{
+      console.log('type something');
+    }
+  } catch (error) {
+      console.log('failed');
+  }
 
 }
 
-searchBtn.addEventListener('click', getRecipe);
+searchBtn.addEventListener('click', () => {
+  const inputRecipe = searchBox.value.trim();
+  getRecipe(inputRecipe);
+});
 
 /*
       
