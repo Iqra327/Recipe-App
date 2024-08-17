f8dc1e9f874e0f590244a00305de45ede910674c
Iqra327 <iqrarasheed219@gmail.com>
2024-08-17 19:05:13 +0500

    in-process

--- script.js
+++ script.js
@@ -1,29 +1,18 @@
 const searchBox = document.querySelector('.js-search-box');
 const searchBtn = document.querySelector('.js-search-btn');
-const viewRecipeBtn = document.querySelector('.js-view-recipe-btn');
 const recipeContainer = document.querySelector('.js-recipe-container');
-
-const html = `
-  <div class="col-lg-3 col-md-4 col-sm-6 mb-3">
-    <div class="card">
-      <img class="card-img-top" src="" alt="Card image cap">
-      <div class="card-body text-center">
-        <h5 class="card-title"></h5>
-        <p class="card-text"></p>
-        <a href="#" class="btn btn-view-recipe js-view-recipe-btn">View Recipe</a>
-      </div>
-    </div>
-  </div>
-`
+const heading = document.querySelector('.js-heading');
 
 async function getRecipe(recipeName){
   try {
+    recipeContainer.innerHTML = ''; 
+  
     if(recipeName.trim() != '')
     {
-      console.log('fetching');
+      heading.innerText = 'Just a few seconds, loading your recipes...';
       const data =await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${recipeName}`);
       const response =await data.json();
-
+  
       response.meals.forEach(meal => {
         const html = `
           <div class="col-lg-3 col-md-4 col-sm-6 mb-3">
@@ -37,23 +26,28 @@ async function getRecipe(recipeName){
             </div>
           </div>
         `
-        recipeContainer.innerHTML += html;       
-      console.log(meal);
+        heading.innerText = '';
+        recipeContainer.innerHTML += html;
       });
-    }else{
-      console.log('type something');
     }
-  } catch (error) {
-      console.log('failed');
+    else{
+      heading.innerText = 'Type a recipe name please!!';
+    }
+  } 
+  catch (error) {
+      heading.innerText = 'An unexpected error occur, Try again.';
   }
+};
 
-}
-
-searchBtn.addEventListener('click', () => {
+searchBtn.addEventListener('click', (e) => {
+  e.preventDefault();
   const inputRecipe = searchBox.value.trim();
   getRecipe(inputRecipe);
 });
 
-/*
-      
-*/
+recipeContainer.addEventListener('click', (e) => {
+  if (e.target && e.target.matches('.js-view-recipe-btn')) {
+    e.preventDefault();
+    
+  }
+});  
